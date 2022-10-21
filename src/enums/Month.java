2d7ebbd6fc03c23a5efd1deed2ece03384f811fe
DimaTivator <andriianov164@gmail.com>
2022-10-21 13:08:34 +0300

    Adding constructor and translation in enums

--- src/enums/Month.java
+++ src/enums/Month.java
@@ -1,16 +1,26 @@
 package enums;
 
 public enum Month {
-    JANUARY,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER;
+    JANUARY("январь"),
+    FEBRUARY("февраль"),
+    MARCH("март"),
+    APRIL("апрель"),
+    MAY("май"),
+    JUNE("июнь"),
+    JULY("июль"),
+    AUGUST("август"),
+    SEPTEMBER("сентябрь"),
+    OCTOBER("октябрь"),
+    NOVEMBER("ноябрь"),
+    DECEMBER("декабрь");
+
+    private final String translation;
+
+    Month(String translation) {
+        this.translation = translation;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
 }
--- src/enums/Weather.java
+++ src/enums/Weather.java
@@ -1,10 +1,20 @@
 package enums;
 
 public enum Weather {
-    EXTREMELY_COLD,
-    COLD,
-    COOL,
-    WARM,
-    HOT,
-    EXTREMELY_HOT;
+    EXTREMELY_COLD("очень холодно"),
+    COLD("холодно"),
+    COOL("прохладно"),
+    WARM("тепло"),
+    HOT("жарко"),
+    EXTREMELY_HOT("очень жарко");
+
+    private final String translation;
+
+    Weather(String translation) {
+        this.translation = translation;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
 }
