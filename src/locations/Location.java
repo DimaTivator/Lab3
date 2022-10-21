1c6cf1ff2fcd6affb917c15a08ac48147a5fb7aa
DimaTivator <andriianov164@gmail.com>
2022-10-21 16:25:14 +0300

    Adding methods that remove objects from lists

--- src/locations/Location.java
+++ src/locations/Location.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public abstract class Location {
 
-    private final Type type = Type.VALE_LOCATION;
+    private final Type type = Type.LOCATION;
 
     public Type getType() {
         return type;
@@ -24,11 +24,39 @@ public abstract class Location {
         return locations;
     }
 
+    /**
+     * removes from the list all objects that have simple the same class with the argument
+     */
+    public void removeLocation(Location object) {
+        int i = 0;
+        while (i < locations.size()) {
+            if (locations.get(i).getClass() == object.getClass()) {
+                locations.remove(i);
+            } else {
+                i++;
+            }
+        }
+    }
+
 
     private ArrayList<MummiValeObject> mummiValeObjects = new ArrayList<>();
 
     public void addMummiValeObject(MummiValeObject object) {
         mummiValeObjects.add(object);
+    }
+
+    /**
+     * removes from the list all objects that have simple the same class with the argument
+     */
+    public void removeMummiValeObject(MummiValeObject object) {
+        int i = 0;
+        while (i < mummiValeObjects.size()) {
+            if (mummiValeObjects.get(i).getClass() == object.getClass()) {
+                mummiValeObjects.remove(i);
+            } else {
+                i++;
+            }
+        }
     }
 
     public ArrayList<MummiValeObject> getMummiValeObjects() {
