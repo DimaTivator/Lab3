7a8fad1936de6e867fc0d50d9221fee591e96ee6
DimaTivator <andriianov164@gmail.com>
2022-11-27 23:16:37 +0300

    part of implementation of class Bird

--- src/valeObjects/Bird.java
+++ src/valeObjects/Bird.java
@@ -5,10 +5,34 @@ import enums.Weather;
 // TODO
 public class Bird extends ValeObject {
 
-    private double volume;
+    // the volume of birdsong
+    private double volume = 3;
 
     @Override
     public void feelWeather(Weather weather) {
+        switch (weather) {
+            case EXTREMELY_HOT -> {
+                volume = 0;
+                sing();
+            }
+            case HOT, WARM -> {
+                volume = 3;
+                sing();
+            }
+            default -> {
+                System.out.println("Мороз. Птицы улетели на юг");
+            }
+        }
+    }
 
+    public void sing() {
+        if (volume > 0) {
+            for (int i = 0; i < volume; i++) {
+                System.out.println("Чик чирик ");
+            }
+            System.out.println(":-)");
+        } else {
+            System.out.println("От жары у птиц нет сил петь :(");
+        }
     }
 }
