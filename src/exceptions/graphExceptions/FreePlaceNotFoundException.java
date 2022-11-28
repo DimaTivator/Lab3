748adac70750195373912c8a53cc4bdbac78829a
DimaTivator <andriianov164@gmail.com>
2022-11-28 18:34:02 +0300

    FreePlaceNotFoundException added

---
+++ src/exceptions/graphExceptions/FreePlaceNotFoundException.java
@@ -0,0 +1,11 @@
+package exceptions.graphExceptions;
+
+// checked exception
+public class FreePlaceNotFoundException extends Exception {
+
+    public FreePlaceNotFoundException(String message) {
+        System.out.println(message);
+    }
+
+    public FreePlaceNotFoundException() {}
+}
