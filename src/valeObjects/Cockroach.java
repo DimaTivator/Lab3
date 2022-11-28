9c252119a3c3ea44719920a7fc0492560ac755bc
DimaTivator <andriianov164@gmail.com>
2022-11-28 16:31:59 +0300

    class CockRoach added

---
+++ src/valeObjects/Cockroach.java
@@ -0,0 +1,99 @@
+package valeObjects;
+
+import abilities.AbleToDig;
+import abilities.AbleToFeelWeather;
+import enums.Weather;
+import locations.Cave;
+
+import java.util.Objects;
+
+public class Cockroach extends Insect implements AbleToFeelWeather, AbleToDig {
+
+    public Cockroach(double sizeX, double sizeY, double sizeZ) {
+        sizes.setSizeX(sizeX);
+        sizes.setSizeY(sizeY);
+        sizes.setSizeZ(sizeZ);
+        System.out.println("Таракан создана");
+    }
+
+    public Cockroach() {
+        System.out.println("Таракан со стандартными размерами создан");
+    }
+
+
+    public void setSizeX(double sizeX) {
+        sizes.setSizeX(Math.max(0, Math.min(10, sizeX)));
+    }
+
+    public void setSizeY(double sizeY) {
+        sizes.setSizeY(Math.max(0, Math.min(10, sizeY)));
+    }
+
+    public void setSizeZ(double sizeZ) {
+        sizes.setSizeZ(Math.max(0, Math.min(10, sizeZ)));
+    }
+
+
+    /**
+     * Cockroach is able to crawl in somebody's pit.
+     * If there is no pits in the cave, cockroach digs a new one.
+     * It is assumed that any cockroach will fit in the smallest pit (x = 1, y = 1, z = 1)
+     */
+    @Override
+    public void dig(Cave cave) {
+        boolean pitFound = false;
+
+        outerCycle:
+        for (int i = 0; i < cave.getSizeX(); i++) {
+            for (int j = 0; j < cave.getSizeY(); j++) {
+                if (cave.getCellValue(i, j) != 0) {
+                    System.out.println(this + " заполз в готовую ямку");
+                    pitFound = true;
+                    break outerCycle;
+                }
+            }
+        }
+
+        if (!pitFound) {
+            outerCycle:
+            for (int i = 0; i < cave.getSizeX(); i++) {
+                for (int j = 0; j < cave.getSizeY(); j++) {
+                    if (cave.getCellValue(i, j) == 0) {
+                        System.out.println(this + " вырыл ямку и заполз в нее");
+                        break outerCycle;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void feelWeather(Weather weather) {
+        System.out.println("Таракану любая погода нипочем");
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("sssss sssss sssss");
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Cockroach fly = (Cockroach) obj;
+        return hashCode() == fly.hashCode() && Objects.equals(sizes.sizeX(), fly.sizes.sizeX())
+                && Objects.equals(sizes.sizeY(), fly.sizes.sizeY()) && Objects.equals(sizes.sizeZ(), fly.sizes.sizeZ());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sizes.sizeX(), sizes.sizeY(), sizes.sizeZ());
+    }
+
+    @Override
+    public String toString() {
+        return "Таракан";
+    }
+}
